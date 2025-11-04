5aff7ef94273c15e67de7df33e339584e37bc6a0
Stanley Chavez <181732770+Pupi143@users.noreply.github.com>
2025-11-04 19:13:01 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,14 @@
 <?php
 $SERVER = "localhost";
 $user = "root";
-$password = "";
+$password = "O/REdP0@5/sv";  // your Bitnami MySQL password
 $db_name = "hotel";
 
 $conn = mysqli_connect($SERVER, $user, $password, $db_name);
+
 if (mysqli_connect_errno()) {
-    die("not connected". mysqli_connect_error());
-
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    // echo "Database connected successfully!";
 }
-
 ?>
