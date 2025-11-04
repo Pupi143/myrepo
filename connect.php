4979b9565cfe2e38c517099752b3bfccd90675da
Stanley Chavez <181732770+Pupi143@users.noreply.github.com>
2025-11-04 19:19:56 +0800

    Fix connection error handling in connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,12 @@
 <?php
 $SERVER = "localhost";
 $user = "root";
-$password = "O/REdP0@5/sv";  // your Bitnami MySQL password
+$password = "O/REdP0@5/sv";  // Bitnami MySQL password
 $db_name = "hotel";
 
 $conn = mysqli_connect($SERVER, $user, $password, $db_name);
 
-if (mysqli_connect_errno()) {
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
     // echo "Database connected successfully!";
