ccbde3990d1675a108714052bb47cbfc5e28f346
Stanley Chavez <181732770+Pupi143@users.noreply.github.com>
2025-11-04 19:56:29 +0800

    Update booking.php

--- booking.php
+++ booking.php
@@ -1,14 +1,5 @@
 <?php
-
 include "connect.php"; // Database connection
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-
-require '/opt/bitnami/apache2/htdocs/phpmailer/src/Exception.php';
-require '/opt/bitnami/apache2/htdocs/phpmailer/src/PHPMailer.php';
-require '/opt/bitnami/apache2/htdocs/phpmailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['button'])) {
@@ -24,45 +15,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bind_param("ssssss", $location, $check_in, $check_out, $room_type, $email, $phone);
 
         if ($stmt->execute()) {
-            // Prepare email
-            try {
-                $mail = new PHPMailer(true);
-
-                // SMTP configuration
-                $mail->isSMTP();
-                $mail->Host = 'smtp.gmail.com';
-                $mail->SMTPAuth = true;
-                $mail->Username = 'transhotel592024@gmail.com';
-                $mail->Password = 'kdpj oxaj ntqo xznx'; 
-                $mail->SMTPSecure = 'tls';
-                $mail->Port = 587;
-
-                // Sender and recipient
-                $mail->setFrom('transhotel592024@gmail.com', 'Reservation System');
-                $mail->addAddress($email); // Send to the user's email address
-
-                // Email content
-                $mail->isHTML(true);
-                $mail->Subject = 'Reservation Confirmation';
-                $mail->Body = "Dear user,<br><br>Thank you for your reservation.<br><br><b>Location:</b> $location<br><b>Check-in:</b> $check_in<br><b>Check-out:</b> $check_out<br><b>Room Type:</b> $room_type<br><b>Email:</b> $email<br><b>Phone:</b> $phone<br><br>Best regards,<br>Hotel Management";
-
-                // Send email
-                $mail->send();
-                echo 'Email sent successfully.';
-            } catch (Exception $e) {
-                echo "Email sending failed. Error: {$mail->ErrorInfo}";
-            }
+            echo "<script>alert('Reservation saved successfully!');</script>";
         } else {
-            echo "Error: " . $stmt->error;
+            echo "<script>alert('Error saving reservation: " . addslashes($stmt->error) . "');</script>";
         }
 
         $stmt->close();
     }
 }
 ?>
-
-
-
 
 <html lang="en">
 
@@ -72,7 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
   <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
   <script type='text/javascript' src='https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js'></script>
-  <script type='text/javascript'></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <script src="navbar.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/annyang/2.6.0/annyang.min.js"></script>
@@ -85,17 +45,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="container">
       <a href="index.php" class="logo"><img src="assets/transhotel_logo1.png" alt="TransHotel Logo"></a>
       <ul class="links">
-        <li> <a href="index.php" style="color: black;">Home</a></li>
-        <li> <a href="index.php" style="color: black;">About Us</a></li>
-        <li> <a href="roomnav.php" style="color: black;">Room</a></li>
-        <li> <a href="facilities.php" style="color: black;">Facilities</a></li>
-        <li> <a href="email.php" style="color: black;">Contact</a></li>
-        <li> <a href="booking.php" style="color: white;">Book Now</a></li>
+        <li><a href="index.php" style="color: black;">Home</a></li>
+        <li><a href="index.php" style="color: black;">About Us</a></li>
+        <li><a href="roomnav.php" style="color: black;">Room</a></li>
+        <li><a href="facilities.php" style="color: black;">Facilities</a></li>
+        <li><a href="email.php" style="color: black;">Contact</a></li>
+        <li><a href="booking.php" style="color: white;">Book Now</a></li>
       </ul>
     </div>
   </header>
 </div><br>
-
 
 <div id="booking" class="section">
   <div class="section-center">
@@ -109,7 +68,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="form-group">
               <input class="form-control" onclick="startListeningDestination()" type="text" name="location"
                 id="destination-input-field" placeholder="Country, ZIP, city...">
-
               <span class="form-label">Destination</span>
             </div>
             <div class="row">
@@ -131,8 +89,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="row">
               <div class="col-md-6">
                 <div class="form-group">
-                  <select class="form-control" name="room_type" id="rooms-input-field" onclick="startListeningRooms()"
-                    required>
+                  <select class="form-control" name="room_type" id="rooms-input-field" onclick="startListeningRooms()" required>
                     <option value="" selected hidden>Rooms</option>
                     <option>Superior Terrace</option>
                     <option>Deluxe Terrace</option>
@@ -156,7 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               </div>
               <div class="col-md-6">
                 <div class="form-group">
-                  <input class="form-control" type="tel" name="phone" placeholder="Enter you Phone"
+                  <input class="form-control" type="tel" name="phone" placeholder="Enter your Phone"
                     id="phone-input-field" onclick="startListeningPhone()">
                   <span class="form-label">Phone</span>
                 </div>
